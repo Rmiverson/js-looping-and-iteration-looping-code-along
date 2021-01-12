0d5733f02d0e0b98930a570cdd68b3fc02252be1
Rmiverson <Rmiverson99@gmail.com>
2021-01-12 09:21:41 -0800

    commit tree a794621d480d50b12fefb8e6499c55ac08f2de03

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const writeCards = (name) => {
+   let ans = []
+   for (let i = 0; i < name.length; i++) {
+      ans.push("Thank you, " + name[i] + ", for the wonderful surprise gift!")
+   }
+   return ans
+}
+
+const countDown = (num) => {
+   let i = num
+
+   while (i >= 0) {
+      console.log(i)
+      i--
+   }
+      
+}
